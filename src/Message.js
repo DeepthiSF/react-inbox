b8ca535c4223fbb040cda40422375c5cf605241d
Deepthi <deepthi.gaddipati.f2jn@statefarm.com>
2021-10-11 11:11:08 -0500

    made some styling changes

--- src/Message.js
+++ src/Message.js
@@ -1,25 +1,25 @@
 const Message = (props) => {
-    return(
+    return (
         <ul>
-                {/* <input type="checkbox" />
+            {/* <input type="checkbox" />
                 <h2> {props.subject} </h2> */}
-                <div class="row message unread">
-  <div class="col-xs-1">
-    <div class="row">
-      <div class="col-xs-2">
-        <input type="checkbox" />
-      </div>
-      <div class="col-xs-2">
-        <i class="star fa fa-star-o"></i>
-      </div>
-    </div>
-  </div>
-  <div class="col-xs-11">
-    <h2> {props.subject} </h2>
-  </div>
-</div>
+            <div class="row message unread">
+                <div class="col-xs-1">
+                    <div class="row">
+                         <div class="col-xs-2">
+                            <input type="checkbox" className='message' />
+                        </div>
+                        <div class="col-xs-2">
+                            <i class="star fa fa-star-o"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-11">
+                    <h2> className='message' {props.subject} </h2>
+                </div>
+            </div>
         </ul>
-        )
+    )
 }
 
 export default Message;
--- src/Toolbar.js
+++ src/Toolbar.js
@@ -5,7 +5,7 @@ const Toolbar = () => {
         <div class="row toolbar">
             <div class="col-md-12">
                 <p class="pull-right">
-                    <span class="badge badge">2</span>
+                    <span class="badge badge" className='badge'>2</span>
                     unread messages
                 </p>
 
