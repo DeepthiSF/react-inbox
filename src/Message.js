f3b87f3c1febc2fab482c382f8ec21ba4b4a1ffe
Deepthi <deepthi.gaddipati.f2jn@statefarm.com>
2021-10-11 10:52:54 -0500

    Added the checkboxes and stars for each message

--- src/Message.js
+++ src/Message.js
@@ -1,8 +1,24 @@
 const Message = (props) => {
     return(
-        <li> 
-            <h2> {props.subject} </h2>
-        </li>
+        <ul>
+                {/* <input type="checkbox" />
+                <h2> {props.subject} </h2> */}
+                <div class="row message unread">
+  <div class="col-xs-1">
+    <div class="row">
+      <div class="col-xs-2">
+        <input type="checkbox" />
+      </div>
+      <div class="col-xs-2">
+        <i class="star fa fa-star-o"></i>
+      </div>
+    </div>
+  </div>
+  <div class="col-xs-11">
+    <h2> {props.subject} </h2>
+  </div>
+</div>
+        </ul>
         )
 }
 
